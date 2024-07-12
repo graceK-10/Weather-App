a2d6fb8cab49c7ad32c06d3f47aca3164ade1876
graceK-10 <kipagrace@gmail.com>
2024-07-12 11:20:26 +0200

    installed axios

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,11 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+// import { useState } from "react";
 import { IoSearchCircleSharp } from "react-icons/io5";
+import { PiWindFill } from "react-icons/pi";
+import { LiaSunSolid } from "react-icons/lia";
+
 import imageData from "./components/Data";
 
 const Home = () => {
   // Find the cloudy image from imageData
   const cloudyImage = imageData.find((image) => image.id === 1);
   const cloudyIcon = imageData.find((image) => image.id === 5);
+
+  //
+  const [data, setData] = useState({
+    celcius: 10,
+    name: "London",
+    humidity: 10,
+    uv: 6.2,
+    uvType: "High",
+  });
+  useEffect(() => {
+    const apiUrl =
+      "https://api.openweathermap.org/data/2.5/weather?q=london&appid=8e5b060c256ec7d7e0aedf990fc00d1c&units=metric";
+    axios
+      .get(apiUrl)
+      .then((res) => console.log(res))
+      .catch((err) => console.log(err));
+  }, []);
 
   return (
     <div className="container">
@@ -26,11 +48,27 @@ const Home = () => {
           {cloudyIcon && (
             <img src={cloudyIcon.src} alt={cloudyIcon.alt} className="mt-10" />
           )}
-          <h1 className="text-5xl m-6">22°C</h1>
-          <h2 className="text-4xl m-10">London</h2>
-          <h3 className="text-xl">Light Rain</h3>
-          <h4 className="text-xl">Humidity: 80%</h4>
-          <h4 className="text-xl">UV Index: 5 (Moderate)</h4>
+          <h1 className="text-5xl mt-6">22°C</h1>
+          <h2 className="text-4xl mt-2">London</h2>
+          <h4 className="text-xl mt-10">Light Rain</h4>
+
+          <div className="details">
+            <div className="col text-xl">
+              <div className="humidity">
+                <PiWindFill className="w-icon" />
+                <p>80%</p>
+                <p>Humidity</p>
+              </div>
+            </div>
+            <div className="col text-xl">
+              <div className="uv-rays">
+                <LiaSunSolid className="w-icon" />
+                <p>5 (Moderate)</p>
+                <p>UV Index</p>
+                {/* <h4 className="text-xl">UV Index: 5 (Moderate)</h4> */}
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
